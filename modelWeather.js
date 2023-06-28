be6abe9b332eb8533d11564fdbf5a64202ba800c
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-06-28 15:54:27 +0700

    create method getDefaultLocation

--- modelWeather.js
+++ modelWeather.js
@@ -1,13 +1,36 @@
 'use strict';
 const dotenv = require('dotenv');
+const axios = require('axios');
 dotenv.config();
 const {
     API_KEY,
     DEFAULT_LOCATION_LATITYDE,
     DEFAULT_LOCATION_LONGITUDE
 } = process.env;
-function getDefaultLocation() {
-    console.log('12');
+const lang = 'vi'
+const units = 'metric'
+//lấy thông tin hn
+async function getDefaultLocation(res, req, next) {
+    try {
+        const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+            params: {
+                lat: DEFAULT_LOCATION_LATITYDE,
+                lon: DEFAULT_LOCATION_LONGITUDE,
+                appid: API_KEY,
+                lang: lang,
+                units: units
+            }
+        });
+
+        const weatherData = response.data;
+        console.log(weatherData);
+        return res.send({ 'code': 200, message: 'success', results: weatherData });
+        // return res.json(weatherDataa);
+    } catch (error) {
+        console.error(error);
+        return res.send({ 'code': 500, message: 'error', results: null });
+        //    return res.status(500).json({ error: 'Something went wrong' });
+    }
 }
 module.exports = {
     getDefaultLocation
--- router.js
+++ router.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const modelAPI = require('./modelWeather');
 
-router.get('/', modelAPI.getDefaultLocation);
+router.get('/', async (res, req, next) => {
+    try {
+        modelAPI.getDefaultLocation(req, res, next)
+    } catch (error) {
+        console.error(error);
+        return res.send({ 'code': 500, message: 'error', results: null });
+    }
+});
 
 module.exports = {
     router
